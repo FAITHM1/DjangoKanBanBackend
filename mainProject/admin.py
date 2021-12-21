da34f71167332b78a87cb64868822b07858e47f8
faith musa <faithy@faiths-MacBook-Air.local>
2021-12-20 19:45:36 -0600

    fixed alot

---
+++ mainProject/admin.py
@@ -0,0 +1,6 @@
+from django.contrib import admin
+from .models import SubProject, MainProject,User
+
+# Register your models here.
+admin.site.register(SubProject)
+admin.site.register(MainProject)
---
+++ mainProject/models.py
@@ -0,0 +1,23 @@
+from django.db import models
+# from django.contrib.auth.models import AbstractBaseUser
+# from django.utils.translation import gettext_lazy as _
+
+# Create your models here.
+class SubProject(models.Model):
+  name = models.CharField(max_length=200)
+  description = models.TextField(max_length=1000)
+  workingOn = models.CharField(max_length=100)
+  mainProjectnum = models.CharField(max_length=10)
+  
+  
+class MainProject(models.Model):
+  title = models.CharField(max_length=200)
+  type = models.CharField(max_length=200)
+  
+  
+# class User(AbstractUser):
+#    username = None
+#     email = models.EmailField(_('email address'), unique=True)
+
+#     USERNAME_FIELD = 'email'
+#     REQUIRED_FIELDS = []
