3c04b0be7942a3d5b11f32df738738459a0bcb81
faith musa <faithy@faiths-MacBook-Air.local>
2021-12-20 19:48:37 -0600

    fixed

--- mainProject/admin.py
+++ mainProject/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import SubProject, MainProject,User
+from .models import SubProject, MainProject
 
 # Register your models here.
 admin.site.register(SubProject)
